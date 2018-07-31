60515cf60a6d53f6c3674b82bdebb23778290e76
Eric Youngson <eayoungs@gmail.com>
2018-07-31 13:59:44 -0700

    Removed old django project files, added .gitignore file

--- search_and_employ/company_search/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CompanySearchConfig(AppConfig):
-    name = 'company_search'
--- search_and_employ/search_and_employ/urls.py
+++ search_and_employ/search_and_employ/urls.py
@@ -15,18 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
-from rest_framework import routers
-from company_search import views
-
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
