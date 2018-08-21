3874626c4e49f35ec3ab0fba506207c41813ceb2
Eric Youngson <eayoungs@gmail.com>
2018-08-21 15:21:02 -0700

    Added Celery debug and project level import

--- search_and_employ/search_and_employ/celery.py
+++ search_and_employ/search_and_employ/celery.py
@@ -1,3 +1,4 @@
+# http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
 from __future__ import absolute_import
 import os
 from celery import Celery
@@ -6,5 +7,10 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'search_and_employ.settings')
 app = Celery('search_and_employ')
 
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
