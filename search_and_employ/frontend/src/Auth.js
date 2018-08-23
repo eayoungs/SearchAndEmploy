4904f799c721d70d25881ba5421398236b2b820b
Eric Youngson <eayoungs@gmail.com>
2018-08-22 21:40:06 -0700

    Revised Auth class name

--- search_and_employ/frontend/src/Auth.js
+++ search_and_employ/frontend/src/Auth.js
@@ -3,7 +3,7 @@ import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import SearchAndEmploy from "./components/SearchAndEmploy";
 import NotFound from "./components/NotFound";
 
-class App extends Component {
+class Auth extends Component {
     render() {
         const authurl = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77arqe48ycj44b&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth&state=2522abcde12345&scope=r_basicprofile";
         return (
